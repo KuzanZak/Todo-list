56b068ae24aa947bd1cd8220911be739bcf9f650
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-09-21 11:31:39 +0200

    on going tasklist

--- App/Controllers/TaskController.php
+++ App/Controllers/TaskController.php
@@ -13,7 +13,7 @@ class TaskController
         $task = new Task;
         $task->getAllNotDone();
         $view = new TaskList([
-            'taskList' => implode('', array_map(fn ($t) => "<li><a href=\"action.php?action=done&id_task=" . $t["id_task"] . "\"><i class=\"fa fa-check-square icon\" aria-hidden=\"true\"></i></a>" . $t["description_task"] . "<span class=\"date-span\">" . getDateFromArray($t["date_reminder"]) . "</span>
+            'taskList' => implode('', array_map(fn ($t) => "<li><a href=\"action.php?action=done&id_task=" . $t["id_task"] . "\" class=\"list-checkbox\"><i class=\"fa fa-check-square icon\" aria-hidden=\"true\"></i></a>" . $t["description_task"] . "<span class=\"date-span\">" . getDateFromArray($t["date_reminder"]) . "</span>
             <div class = \"list-links\">" . $t["date_reminder"] . " <a href=\"taskListModify.php?action=modify&id_task=" . $t["id_task"] . "\" class=\"link-modify\"><i class=\"fa fa-commenting-o link-comments\" aria-hidden=\"true\"></i></a>
             <a href=\"action.php?action=delete&id_task=" . $t["id_task"] . "\" class=\"link-delete\"><i class=\"fa fa-trash link-comments delete-icon\" aria-hidden=\"true\"></i></a>
             <div class=\"priority-modification\"><a href=\"action.php?action=up&id_task=" . $t["id_task"] . "\" class=\"link-up\"><i class=\"fa fa-caret-up link-comments up-caret\" aria-hidden=\"true\"></i></a>
